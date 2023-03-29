f7bd5e4f3a7bde870ff126561ca128bc1a0552bb
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2023-03-29 14:00:24 +0000

    1

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    for(int i = 0; i<5; i++){
+      for(int j=0; j < 5 - i; j++)
+        System.out.print(" ");
+      for(int j=0; j < i+1; j++)
+        System.out.print("*");
+      System.out.println();
+    }
   }
 }
